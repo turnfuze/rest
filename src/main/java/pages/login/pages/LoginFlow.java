a950c3ec89f11d0225a2eab036a3938d2e4885ea
Yaroslav <muontarn@gmail.com>
2022-06-15 17:55:41 +0300

    test

--- src/main/java/pages/login/pages/LoginFlow.java
+++ src/main/java/pages/login/pages/LoginFlow.java
@@ -1,15 +1,15 @@
-package pages.login;
+package pages.login.pages;
 
 import org.openqa.selenium.WebDriver;
 import pages.AbstractPage;
-import pages.login.pages.LoginPopup;
 
 public class LoginFlow extends AbstractPage {
-    private LoginPopup loginPopup = new LoginPopup(driver);
+    private LoginPopup loginPopup;
 
 
     public LoginFlow(WebDriver driver) {
         super(driver);
+        loginPopup = new LoginPopup(driver);
     }
 
     public void enterLogin(String login) {
@@ -25,7 +25,12 @@ public class LoginFlow extends AbstractPage {
     public void clickSubmit() {
         loginPopup.getSubmit().click();
     }
+
     public boolean checkErrorAlert(){
         return loginPopup.getErrorAlert().isDisplayed();
     }
+
+    public void clickSignUp(){loginPopup.getSignUp().click();}
+
+    public boolean checkSignUpBtn(){return loginPopup.getCheckSignUp().isDisplayed();}
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,18 +1,20 @@
 import driver.cash.WebDriverCash;
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import pages.base.HomeFlow;
 import pages.base.HomePage;
-import pages.login.LoginFlow;
+import pages.login.pages.LoginFlow;
 import pages.login.pages.LoginPopup;
 
-public class MyTest {
+public class LoginTest {
     private LoginFlow loginFlow;
     private WebDriver webDriver;
     private HomePage homePage;
     private LoginPopup loginPopup;
+    private HomeFlow homeFlow;
 
     @BeforeEach
     public void start() {
@@ -20,24 +22,31 @@ public class MyTest {
         loginFlow = new LoginFlow(webDriver);
         homePage = new HomePage(webDriver);
         loginPopup = new LoginPopup(webDriver);
+        homeFlow = new HomeFlow(webDriver);
     }
 
     @Test
-    public void login() throws InterruptedException {
+    public void loginError(){
         webDriver.get("https://rozetka.com.ua/");
         webDriver.manage().window().maximize();
-        homePage.getSign().click();
-        Thread.sleep(2000);
+        homeFlow.clickSign();
         loginFlow.enterLogin("Taynik");
         loginFlow.enterPassword("Sisam-Open");
         loginFlow.clickSubmit();
-        loginPopup.getErrorAlert();
+        Assertions.assertTrue(loginFlow.checkErrorAlert());
     }
 
-    @SneakyThrows
+    @Test
+    public void signUp(){
+        webDriver.get("https://rozetka.com.ua/");
+        webDriver.manage().window().maximize();
+        homeFlow.clickSign();
+        loginFlow.clickSignUp();
+        Assertions.assertTrue(loginFlow.checkSignUpBtn());
+    }
+
     @AfterEach
     public void clear() {
-        Thread.sleep(3000);
         webDriver.quit();
     }
 }
